7faf8b682d474e51e3c59731b92ad2807b59325c
Eric Fredericksen <eric.e.fredericksen@gmail.com>
2022-01-25 20:08:44 -0500

    Added instance type mapping

--- Terraform_provider/main.tf
+++ Terraform_provider/main.tf
@@ -1,6 +1,15 @@
-resource "vast_instance" "contract" {
+
+
+resource "example_server" "my-server-name" {
 	command = "create instance"
     program = "./vast"
-    arguments = "instance ID Here"
-    api_key = API_KEY
+    arguments = ""
+    api_key = var.api_key
+    instance_type = "DLperf_high" # Options = DLperf_high, DLperf_med, DLperf_low
+    
 }
+
+output "contract" {
+  value = "${example_server.my-server-name.contract}"
+}
+
--- Terraform_provider/resource.go
+++ Terraform_provider/resource.go
@@ -19,17 +19,30 @@ func resourceServer() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"program": &schema.Schema{
 				Type:     schema.TypeString,
+				ForceNew: true,
 				Required: true,
 			},
 			"command": &schema.Schema{
 				Type:     schema.TypeString,
+				ForceNew: true,
 				Required: true,
 			},
 			"arguments": &schema.Schema{
 				Type:     schema.TypeString,
-				Required: true,
+				ForceNew: true,
+				Optional: true,
 			},
 			"api_key": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"contract": &schema.Schema{
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Optional: true,
+			},
+			"instance_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
@@ -40,19 +53,37 @@ func resourceServer() *schema.Resource {
 func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
 	program := d.Get("program").(string)
 	command := d.Get("command").(string)
-	args := d.Get("arguments").(string)
+	instance_type := d.Get("instance_type").(string)
+	query := ""
 
 	authenticate(d, m)
-
-	contract, resp, err := cli("./vast", program, command, args)
-
-	d.SetId(contract)
-
-	if err != "" {
-		log.Print(err)
+	if strings.Contains(instance_type, "DLperf_high") {
+		query = "'reliability > 0.99  num_gpus=4 gpu_ram > 24 total_flops > 100 pcie_bw > 10 cpu_ram > 127'"
+	} else if strings.Contains(instance_type, "DLperf_med") {
+		query = "'reliability > 0.99  num_gpus=2 gpu_ram > 12 total_flops > 75 pcie_bw > 10 cpu_ram > 127'"
+	} else {
+		query = "'reliability > 0.99  num_gpus=1 gpu_ram > 8 total_flops > 50 pcie_bw > 10 cpu_ram > 127'"
 	}
 
-	fmt.Print(resp)
+	if query != "" {
+		args, err1 := cliSearch("./vast", program, "search offers", query)
+
+		if err1 != "" {
+			log.Print(err1)
+		}
+
+		contract, resp, err := cli("./vast", program, command, args)
+
+		d.SetId(contract)
+		d.Set("contract", contract)
+
+		if err != "" {
+			log.Print(err)
+		}
+
+		fmt.Print(resp)
+
+	}
 
 	return resourceServerRead(d, m)
 }
