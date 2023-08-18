a3b10389835e76a10e48bf30e22f702196a9e0a1
Nick Greenspan <nicholasg101@gmail.com>
2023-08-18 11:45:48 -0400

    added script to stop (not destroy) all instances

---
+++ autoscaler-py/stop_instances.py
@@ -0,0 +1,9 @@
+from autoscaler import InstanceSet
+
+def main():
+    instances = InstanceSet(manage=False)
+    instances.stop_all_instances()
+    instances.deconstruct()
+
+if __name__ == "__main__":
+	main()
