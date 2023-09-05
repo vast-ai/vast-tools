56c657db19148ea20efd8a04ce63cb6304768afb
Nick Greenspan <nicholasg101@gmail.com>
2023-09-05 16:05:54 -0400

    added util functions

---
+++ autoscaler-py/create_instances.py
@@ -0,0 +1,21 @@
+from autoscaler import InstanceSet
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str)
+parser.add_argument("--num_instances", type=int)
+args = parser.parse_args()
+
+if args.model:
+    model = args.model
+else:
+    model = "vllm-70"
+
+if args.num_instances:
+    num_instances = args.num_instances
+else:
+    num_instances = 1
+
+i_set = InstanceSet(manage=False)
+i_set.create_instances(num_instances=num_instances, model=model)
+i_set.deconstruct()
--- autoscaler-py/sim.py
+++ autoscaler-py/sim.py
@@ -18,9 +18,6 @@ PROMPTS = [ "Yesterday I woke up and I saw that my dog was missing. This made me
 			"In the realm of possibilities, where ideas take shape and dreams find expression, there exists a tapestry of thoughts that intertwine and weave the fabric of our understanding. From the cosmic dance of stars in the night sky to the gentle rustle of leaves in an ancient forest, the universe unfolds its mysteries before our curious gaze. In this vast expanse, the human spirit embarks on a quest for knowledge, driven by an insatiable thirst to unravel the enigma of existence. As we navigate the corridors of time, we are bound by the threads of history that have shaped civilizations and cultures across continents. From the pyramids of Egypt that stand as sentinel monuments to human ingenuity to the intricate temples of Angkor Wat that speak of devotion carved in stone, the echoes of the past reverberate in the present, reminding us of the enduring legacy of our predecessors. The passage of time has ushered in waves of change, ushering in revolutions that have redefined the course of human progress. The clanking gears of the Industrial Revolution gave rise to a new era of mechanization, propelling society into the modern age. Today, the digital revolution weaves a digital tapestry that connects us in ways previously unimaginable, transcending geographical boundaries and fostering a global village of interconnected minds. Amidst the grandeur of nature and the innovations of technology, the human experience is a mosaic of emotions that paint the canvas of our lives. Love, joy, sorrow, and resilience converge to create a symphony of feelings that accompany us through the journey of existence. It is in the embrace of loved ones, the laughter of friends, and the solace of solitude that we find moments of true meaning. Language, the bridge between minds, empowers us to share thoughts, emotions, and ideas with fellow travelers on this cosmic journey. From the verses of ancient poets to the prose of modern storytellers, words have the power to transcend time and touch the souls of generations. Through the written word, we communicate the collective wisdom of humanity, passing down knowledge from one era to the next. The pursuit of knowledge fuels the fires of curiosity that burn within us. Science unfurls the mysteries of the universe, revealing the intricate dance of particles and the cosmic ballet of galaxies. Artistic expression, in its myriad forms, offers a window into the human imagination, capturing the essence of beauty and complexity that define our world. In this grand tapestry of existence, each thread represents a life, each moment a fleeting brushstroke on the canvas of eternity. As we tread the paths of our own narratives, we contribute to the evolving story of humanity, leaving footprints that echo in the corridors of time. The interplay of individual lives and collective destinies weaves a narrative that transcends generations, serving as a testament to the indomitable spirit of the human journey."
 ]
 
-class ThreadTermination(Exception):
-    pass
-
 class User:
 	def __init__(self, id, rate, prompt=PROMPTS[0], max_response_length=200):
 		self.id = id
@@ -140,7 +137,7 @@ class Sim:
 		self.client.shutdown_lb()
 
 def main():
-	sim = Sim(num_iters=100, base_num_users=100, base_rate=1.0 * (15 / 60), etime=2.0)
+	sim = Sim(num_iters=100, base_num_users=200, base_rate=1.0 * (15 / 60), etime=2.0)
 	sim.run()
 
 if __name__ == "__main__":
